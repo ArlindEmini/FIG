a19d64dae258333bee0593498cb861c4485b8a11
arlind <arlind.emini7@gmail.com>
2022-04-07 12:36:43 +0200

    Implemented login and jsonwebtoken generated

--- service/controllers/users-controllers.js
+++ service/controllers/users-controllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const HttpError = require("../models/http-error");
 const Users = require("../models/Users");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { name, password } = req.body;
@@ -44,7 +45,49 @@ const login = async (req, res, next) => {
     const error = new HttpError("User doesn't exist", 500);
     return next(error);
   }
-  res.json("Logged In");
+
+  let isValidPassword = false;
+
+  try {
+    isValidPassword = await bcrypt.compare(password, user.password);
+  } catch (err) {
+    const error = HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      403
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { id: user.id, fullName: user.fullname },
+      "jsonwebtokensecret",
+      {
+        expiresIn: "1h",
+      }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({
+    userId: user.id,
+    name: user.fullName,
+    token: token,
+  });
 };
 
 exports.signup = signup;
