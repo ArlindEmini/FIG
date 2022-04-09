6e3a67dcfa4f7042bb5fd82f31d577b4d3abcc97
alimorina <alimorina@hotmail.com>
2022-04-09 23:00:03 +0200

    simulate loader in customer details

--- frontend/src/routes/ClientDetails/index.js
+++ frontend/src/routes/ClientDetails/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import {  Text, ScrollView} from "react-native";
+import { ActivityIndicator } from 'react-native-paper';
 
 import CustomCard from "../../components/Card";
 import { styles } from "./styles";
@@ -8,11 +9,15 @@ import { styles } from "./styles";
 function ClientDetails({ route }) {
 
   const { params } = route;
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     // ... fetch client details
+    console.log({ params })
+    setTimeout(() => setLoading(false), 5000);
   })
+
+  if(loading) return (<ActivityIndicator animating={true} style={styles.loader} size="large" />)
 
   return (
     <>
@@ -22,29 +27,6 @@ function ClientDetails({ route }) {
         </CustomCard>
 
         <CustomCard showActions={false} subTitle={""} title={"Cahier des charges"}>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
-          <Text>Tonte du gazon.. 0/15</Text>
           <Text>Tonte du gazon.. 0/15</Text>
           <Text>Tonte du gazon.. 0/15</Text>
           <Text>Tonte du gazon.. 0/15</Text>
