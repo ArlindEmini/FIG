3578d2906d01eb802173dfffc89b7e610bbf9d18
alimorina <alimorina@hotmail.com>
2022-04-09 22:42:02 +0200

    import useState

--- frontend/src/routes/Client/ClientList.js
+++ frontend/src/routes/Client/ClientList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { List, Text } from 'react-native-paper';
 import IonIcons from 'react-native-vector-icons/Ionicons';
 import { warning } from '../../utils/colors';
