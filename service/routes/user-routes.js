c00a0fc18bfee4bef5f3397edb48ef1a3cac6f2f
Arlind Emini <arlind.emini@sqa-consulting.com>
2023-01-02 19:27:25 +0100

    improved time off apis

--- service/routes/user-routes.js
+++ service/routes/user-routes.js
@@ -13,6 +13,7 @@ import validateUser from "../validators/user-validator.js";
 
 const router = express.Router();
 
+
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -45,10 +46,141 @@ router.post("/login", async (req, res) => {
       })
       .end();
   } catch (error) {
-    console.log(error);
+    
     return res.status(400).json({ error }).end();
   }
 });
+
+// get all users
+router.get("/", authenticateToken, async (req, res) => {
+  
+  try {
+    const { query, headers } = req;
+
+    const response = await UserController.fetchAll(query);
+
+    return res.status(200).json({ response }).end();
+  } catch (error) {
+    
+    return res.status(400).json({ error }).end();
+  }
+});
+
+// create new employee
+router.post("/", authenticateToken, async (req, res) => {
+  try {
+    const { body, headers } = req;
+    validateUser(body);
+
+    if (!(await validateAdmin(headers.authorization))) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised action for this user" })
+        .end();
+    }
+
+    const user = await UserController.create(body);
+   
+
+    return res.status(200).json({ user }).end();
+  } catch (error) {
+    
+    return res.status(400).json({ error }).end();
+  }
+});
+
+// edit employee
+router.put("/:id", authenticateToken, async (req, res) => {
+ 
+  try {
+    const { body, headers, params } = req;
+    const { id } = params;
+
+    if (!(await validateAdmin(headers.authorization))) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised action for this user" })
+        .end();
+    }
+
+    const existingUser = await UserController.get(id);
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "Unable to find the user" }).end();
+    }
+
+    const user = await UserController.update(id, body, existingUser);
+
+    return res.status(200).json({ user }).end();
+  } catch (error) {
+    return res.status(400).json({ error }).end();
+  }
+});
+
+// get timeoff requests by the person id, who made the request, id is taken from token
+router.get("/time-off", authenticateToken, async (req, res) => {
+
+  try {
+    const { params, headers } = req;
+
+    const idFromtoken = await getIdFromToken(headers.authorization);
+    
+    const pto = await UserController.getPtoByUserId(idFromtoken);
+
+    return res.status(200).json({ pto }).end();
+  } catch (error) {
+   
+    return res.status(400).json({ error }).end();
+  }
+});
+
+// get employee by id
+router.get("/:id", authenticateToken, async (req, res) => {
+  try {
+    const { params, headers } = req;
+    const { id } = params;
+    const idFromtoken = await getIdFromToken(headers.authorization);
+    
+    const user = await UserController.get(id);
+
+    if (!user) {
+      return res.status(404).json({ error: "Unable to find the user" }).end();
+    }
+
+    return res.status(200).json({ user }).end();
+  } catch (error) {
+    
+    return res.status(400).json({ error }).end();
+  }
+});
+
+// delete employee
+router.delete("/:id", authenticateToken, async (req, res) => {
+  try {
+    const { params, headers } = req;
+    const { id } = params;
+
+    if (!(await validateAdmin(headers.authorization))) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised action for this user" })
+        .end();
+    }
+
+    const existingUser = await UserController.get(id);
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "Unable to find the user" }).end();
+    }
+
+    await UserController.delete(id);
+
+    return res.status(200).json({ res: "User deleted successfully" }).end();
+  } catch (error) {
+    return res.status(400).json({ error }).end();
+  }
+});
+
 
 // kur admini ben kerkese per puntorin
 router.post("/:id/time-off", authenticateToken, async (req, res) => {
@@ -56,7 +188,7 @@ router.post("/:id/time-off", authenticateToken, async (req, res) => {
     const { body, params, headers } = req;
     const { id } = params;
 
-    console.log("number of days", body.number_of_days);
+   
 
     if (!(await validateAdmin(headers.authorization))) {
       return res
@@ -76,10 +208,12 @@ router.post("/:id/time-off", authenticateToken, async (req, res) => {
 
     return res.status(200).json({ pto }).end();
   } catch (error) {
-    console.log("error", error);
+    
     return res.status(400).json({ error }).end();
   }
 });
+
+
 
 // kur puntori bon kerkese vet
 router.post("/time-off", authenticateToken, async (req, res) => {
@@ -108,20 +242,6 @@ router.post("/time-off", authenticateToken, async (req, res) => {
   }
 });
 
-router.get("/time-off", authenticateToken, async (req, res) => {
-  try {
-    const { params, headers } = req;
-
-    const idFromtoken = await getIdFromToken(headers.authorization);
-    console.log("idFromtoken", idFromtoken);
-    const pto = await UserController.getPtoByUserId(idFromtoken);
-
-    return res.status(200).json({ pto }).end();
-  } catch (error) {
-    return res.status(400).json({ error }).end();
-  }
-});
-
 // admin to accept or not timeOff requests
 router.put("/time-off/:tid", authenticateToken, async (req, res) => {
   try {
@@ -138,114 +258,6 @@ router.put("/time-off/:tid", authenticateToken, async (req, res) => {
     const updatedPto = await UserController.updateTimeOffStatus(tid, status);
 
     return res.status(200).json({ updatedPto }).end();
-  } catch (error) {
-    return res.status(400).json({ error }).end();
-  }
-});
-
-router.get("/", authenticateToken, async (req, res) => {
-  console.log("44");
-  try {
-    const { query, headers } = req;
-
-    const response = await UserController.fetchAll(query);
-
-    return res.status(200).json({ response }).end();
-  } catch (error) {
-    console.log("error", error);
-    return res.status(400).json({ error }).end();
-  }
-});
-
-router.post("/", authenticateToken, async (req, res) => {
-  try {
-    const { body, headers } = req;
-    validateUser(body);
-
-    if (!(await validateAdmin(headers.authorization))) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorised action for this user" })
-        .end();
-    }
-
-    const user = await UserController.create(body);
-    console.log("user", user);
-
-    return res.status(200).json({ user }).end();
-  } catch (error) {
-    console.log("ERROr", error);
-    return res.status(400).json({ error }).end();
-  }
-});
-
-router.put("/:id", authenticateToken, async (req, res) => {
-  console.log("22");
-  try {
-    const { body, headers, params } = req;
-    const { id } = params;
-
-    if (!(await validateAdmin(headers.authorization))) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorised action for this user" })
-        .end();
-    }
-
-    const existingUser = await UserController.get(id);
-
-    if (!existingUser) {
-      return res.status(404).json({ error: "Unable to find the user" }).end();
-    }
-
-    const user = await UserController.update(id, body, existingUser);
-
-    return res.status(200).json({ user }).end();
-  } catch (error) {
-    return res.status(400).json({ error }).end();
-  }
-});
-
-router.get("/:id", authenticateToken, async (req, res) => {
-  try {
-    const { params, headers } = req;
-    const { id } = params;
-    const idFromtoken = await getIdFromToken(headers.authorization);
-    
-    const user = await UserController.get(id);
-
-    if (!user) {
-      return res.status(404).json({ error: "Unable to find the user" }).end();
-    }
-
-    return res.status(200).json({ user }).end();
-  } catch (error) {
-    console.log("ERROr", error);
-    return res.status(400).json({ error }).end();
-  }
-});
-
-router.delete("/:id", authenticateToken, async (req, res) => {
-  try {
-    const { params, headers } = req;
-    const { id } = params;
-
-    if (!(await validateAdmin(headers.authorization))) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorised action for this user" })
-        .end();
-    }
-
-    const existingUser = await UserController.get(id);
-
-    if (!existingUser) {
-      return res.status(404).json({ error: "Unable to find the user" }).end();
-    }
-
-    await UserController.delete(id);
-
-    return res.status(200).json({ res: "User deleted successfully" }).end();
   } catch (error) {
     return res.status(400).json({ error }).end();
   }
