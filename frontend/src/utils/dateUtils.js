a22f0cf5f5f42925f7574eb04929850d20cfb07f
Arlind Emini <arlind.emini@sqa-consulting.com>
2023-02-21 14:05:11 +0100

    removed front end folder

--- frontend/src/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-const timeToString = (time) => {
-  const date = new Date(time);
-  
-  return date.toISOString().split('T')[0];
-}
-
-module.exports = {
-  timeToString
-}
--- frontend/src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jwt-decode";
-
-export const verifyToken = (token) => {
-  try {
-    return jwt(token);
-  } catch (error) {
-    return false;
-  }
-}
