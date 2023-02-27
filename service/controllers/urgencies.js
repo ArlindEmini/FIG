046204157f7cfe66e5b327f5c56cef8594036ec6
Arlind Emini <arlind.emini@sqa-consulting.com>
2023-02-27 12:32:29 +0100

    added urgencies

---
+++ service/controllers/urgencies.js
@@ -0,0 +1,79 @@
+import bcrypt from "bcrypt";
+import { QueryTypes } from "sequelize";
+
+import {fetchAllUrgencies,
+        insertUrgencyQuery,
+        getUrgencyById,
+        updateUrgencyQuery      
+    }
+         from "../database/queries.js";
+import { database } from "../database/connection.js";
+
+export default class UrgencyService {
+
+    static get = async (
+		id,
+	) => {
+		const urgencies = await database.query(
+			getUrgencyById,
+			{
+				replacements: {
+					id
+				},
+				type: QueryTypes.SELECT,
+				raw: true,
+			},
+		);
+
+		return urgencies.length ? urgencies[0] : null;
+	};
+
+    static fetchAll = async (query) => {
+        // const { username, email } = query;
+        // let customQuery = fetchAllQuery;
+    
+        // if (username) {
+        //   customQuery += ` AND username = '${username}'`;
+        // }
+    
+        // if (email) {
+        //   customQuery += ` AND email = '${email}'`;
+        // }
+    
+        return await database.query(fetchAllUrgencies, {
+          type: QueryTypes.SELECT,
+        });
+      };
+
+      static createUrgency = async (body) => {
+        const { urgency_description, address, price } = body;
+        
+          return await database.query(insertUrgencyQuery, {
+            replacements: {
+                urgency_description,
+                address,
+                price,
+            },
+            type: QueryTypes.INSERT,
+          });
+      };
+
+      static update = async (
+		id,
+		body,
+        existingUrgency
+	) => {
+		await database.query(
+			updateUrgencyQuery,
+			{
+				replacements: {
+					id,
+					urgency_description: body.urgency_description || existingUrgency.urgency_description,
+					address: body.address || existingUrgency.address,
+					price: body.price || existingUrgency.price
+				},
+				type: QueryTypes.UPDATE,
+			},
+		);
+	};
+}
---
+++ service/routes/urgencies-routes.js
@@ -0,0 +1,116 @@
+import express from "express";
+
+import UrgencyController from "../controllers/urgencies.js";
+import HttpError from "../models/http-error.js";
+import {
+  generateToken,
+  validatePassword,
+  validateAdmin,
+  getIdFromToken,
+  
+} from "../utils/utils.js";
+import authenticateToken from "../controllers/authentication.js";
+import validateUser from "../validators/user-validator.js";
+
+
+const router = express.Router();
+
+router.get("/", async (req,res) => {
+    try {
+        // const { query, headers } = req;
+    
+        const response = await UrgencyController.fetchAll();
+    
+        return res.status(200).json({ response }).end();
+      } catch (error) {
+        
+        return res.status(400).json({ error }).end();
+      }
+})
+
+router.post("/", authenticateToken, async (req, res) => {
+    try {
+      const { body, headers } = req;
+    //   validateUser(body);
+  
+      if (!(await validateAdmin(headers.authorization))) {
+        return res
+          .status(401)
+          .json({ error: "Unauthorised action for this user" })
+          .end();
+      }
+  
+      const urgency = await UrgencyController.createUrgency(body);
+     
+  
+      return res.status(200).json({ urgency }).end();
+    } catch (error) {
+      console.log("error", error)
+      return res.status(400).json({ error }).end();
+    }
+  });
+
+  router.get("/:id", authenticateToken, async (req, res) => {
+    try {
+      const { params, headers } = req;
+      const { id } = params;
+  
+      if (!(await validateAdmin(headers.authorization))) {
+        return res
+        .status(404)
+        .json({ error: "This operation is not allowed for this user" })
+        .end();
+      }
+  
+      const urgency = await UrgencyController.get(id);
+  
+      if (!urgency) {
+        return res
+          .status(404)
+          .json({ error: "Unable to find the urgency" })
+          .end();
+      }
+  
+      return res.status(200).json({ urgency }).end();
+    } catch (error) {
+      return res.status(400).json({ error }).end();
+    }
+  });
+
+  router.put("/:id", authenticateToken, async (req, res) => {
+  try {
+    const { body, headers, params } = req;
+    const { id } = params;
+
+    if (!(await validateAdmin(headers.authorization))) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised action for this user" })
+        .end();
+    }
+
+    const existingUrgency = await existingUrgency.get(id);
+
+    if (!existingUrgency) {
+      return res
+        .status(404)
+        .json({ error: "Unable to find the urgency" })
+        .end();
+    }
+
+    const urgency = await UrgencyController.update(
+      id,
+      body,
+      existingUrgency
+    );
+
+    return res.status(200).json({ urgency }).end();
+  } catch (error) {
+    console.log("ERR", error);
+    return res.status(400).json({ error }).end();
+  }
+});
+
+
+
+export default router;
